437a73aee6f5b409b8030b6ff3fad70a2e28b12b
sol_jung <soljung0725@gmail.com>
2024-01-02 13:52:12 +0900

    test 수정

--- src/test/java/com/example/jpaboard/JpaboardApplicationTests.java
+++ src/test/java/com/example/jpaboard/JpaboardApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.jpaboard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class JpaboardApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
